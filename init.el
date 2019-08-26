162a6dbadda7d30226648d9b1fd9ceecb76a7d9b
xinhao.chen <xinhao.chen@mobiliya.com>
2019-08-26 10:58:20 +0800

    format

--- init.el
+++ init.el
@@ -62,7 +62,7 @@ This function should only modify configuration layer settings."
      (shell :variables
             shell-default-height 30
             shell-default-position 'bottom
-	    shell-default-shell 'eshell)
+            shell-default-shell 'eshell)
 
      spell-checking
      syntax-checking
